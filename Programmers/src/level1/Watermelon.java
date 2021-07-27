91a5a63ee78c557bb33499e2399bafda4fd45e4c
ohj4312 <ohj4312@naver.com>
2021-07-28 00:54:22 +0900

    level1-수박수박수박수박수?

---
+++ Programmers/src/level1/Watermelon.java
@@ -0,0 +1,19 @@
+package level1;
+
+public class Watermelon {
+	public static void main(String[] args) {
+		System.out.println(solution(3));
+		System.out.println(solution(4));
+	}
+    public static String solution(int n) {
+        String answer = "";
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                answer+="수";
+            }else{
+                answer+="박";
+            }
+        }
+        return answer;
+    }
+}
